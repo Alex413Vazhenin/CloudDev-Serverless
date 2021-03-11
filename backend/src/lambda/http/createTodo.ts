0c7dfcf0d1ba0f9f91a0ec3aeaac9c5144f5c072
Alexander Vazhenin <a.vazhenin413@gmail.com>
2021-03-11 20:06:28 +0500

    Added posible interactions with todos

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -8,5 +8,19 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
 
   // TODO: Implement creating a new TODO item
-  return undefined
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+
+  const newItem = await createTodo(newTodo, jwtToken)
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      item: newItem
+    })
+  }
 }
