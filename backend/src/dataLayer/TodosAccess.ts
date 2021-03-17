eaa2021424b461a749ba2b48318c9cf60b5739f9
Alexander Vazhenin <a.vazhenin413@gmail.com>
2021-03-17 12:23:57 +0500

    Added updateTodoRequest function

--- backend/src/dataLayer/TodosAccess.ts
+++ backend/src/dataLayer/TodosAccess.ts
@@ -9,12 +9,43 @@ const XAWS = AWSXRay.captureAWS(AWS)
 import { createLogger } from '../utils/logger'
 const logger = createLogger('dataLayer')
 
-
 import { TodoItem } from '../models/TodoItem'
 import { S3 } from 'aws-sdk'
 
 export class TodosAccess {
+    async updateTodo(update: UpdateTodoRequest, userId: string, todoId: string ): Promise<String> {
+        const { name,dueDate,done } = update
+        const params = {
+            TableName: this.todosTable,
+            Key:                  
+              {todoId,
+              userId},
+            UpdateExpression: "set #name=:n, #dueDate=:dD, #done=:d",
+            ExpressionAttributeValues: {
+              ':n': name,
+              ':dD': dueDate,
+              ':d': done
+            },
+            ExpressionAttributeNames: {
+              '#name': 'name',
+              '#dueDate': 'dueDate',
+              '#done': 'done'
+            },
+            ReturnValues:"UPDATED_NEW"       
+        };
 
+        logger.info('Item update in progress ...', {
+            // Additional info added in logs
+            userId
+        })
+
+        await this.docClient.update(params, function(err) {
+            if (err) {
+              logger.info("Item update failed. ", {message: err.message});
+            } else {
+              logger.info("Item has been sucessfully updated.");
+            }
+        }).promise();
 }
 
 
